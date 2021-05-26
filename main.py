bc57ee31af76229c0f2651d5e2d9a300b52c71cc
Malabika Sen <malabika1310@gmail.com>
2021-05-25 20:50:40 -0400

    updated logging

--- main.py
+++ main.py
@@ -66,9 +66,9 @@ def main():
 
     for idx in range(len(evaluation.runners)):
         start_time = time.process_time()
-        logging.info("Training started for %s with randSeed=%s, hidden layers=%s"%(evaluation.runners[idx].params['encoder']+'-'+evaluation.runners[idx].params['decoder'], evaluation.runners[idx].randSeed, evaluation.runners[idx].params['hidden']))
+        logging.info("Training started with randSeed=%s, hidden layers=%s"%(evaluation.runners[idx].randSeed))
         evaluation.runners[idx].run()
-        logging.info("Training for %s with randSeed=%s completed in %.3f seconds"%(evaluation.runners[idx].params['encoder']+'-'+evaluation.runners[idx].params['decoder'], evaluation.runners[idx].randSeed, time.process_time()-start_time))
+        logging.info("Training for randSeed=%s completed in %.3f seconds"%(evaluation.runners[idx].randSeed, time.process_time()-start_time))
         
     for idx in range(len(evaluation.runners)):
         evaluation.runners[idx].parseOutput()
